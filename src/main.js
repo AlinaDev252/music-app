52dc64a60e0628d7062c1c95f880aa11aa1906af
AlinaDev252 <costachealina25@gmail.com>
2021-07-16 16:21:02 +0300

    Add configuration for Vee Validate library

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VeeValidatePlugin from './includes/validation';
 import './assets/tailwind.css';
 import './assets/main.css';
 
@@ -9,5 +11,6 @@ const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.use(VeeValidatePlugin);
 
 app.mount('#app');
