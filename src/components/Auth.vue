32abdd6c8ca286b151f70ede7110c6c33dc9e29b
AlinaDev252 <costachealina25@gmail.com>
2021-07-27 14:59:21 +0300

    Add an alert box to the template & update the register method

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -87,6 +87,9 @@
 						</button>
 					</form>
 					<!-- Registration Form -->
+					<div class="text-white text-center font-bold p-5 mb-4" v-if="reg_show_alert" :class="reg_alert_variant">
+						{{ reg_alert_msg }}
+					</div>
 					<vee-form
 						v-show="tab === 'register'"
 						:validation-schema="schema"
@@ -185,6 +188,7 @@
 						</div>
 						<button
 							type="submit"
+							:disabled="reg_in_submission"
 							class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
                 hover:bg-purple-700"
 						>
@@ -232,6 +236,13 @@ export default {
   methods: {
     ...mapMutations(['toggleAuthModal']),
     register(values) {
+      this.reg_show_alert = true;
+      this.reg_in_submission = true;
+      this.reg_alert_variant = 'bg-blue-500';
+      this.reg_alert_msg = 'Please wait! Your account is being created.';
+
+      this.reg_alert_variant = 'bg-green-500';
+      this.reg_alert_msg = 'Success! Your account has been created!';
       console.log(values);
     },
   },
