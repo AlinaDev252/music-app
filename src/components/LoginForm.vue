f1689417fb60972215c6a3909e70aa9ef1691223
AlinaDev252 <costachealina25@gmail.com>
2021-07-28 15:28:38 +0300

    Add the alert box for the LoginForm component

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,5 +1,8 @@
 <template>
 	<!-- Login Form -->
+	<div class="text-white text-center font-bold p-4 mb-4" v-if="login_show_alert" :class="login_alert_variant">
+		{{ login_alert_msg }}
+	</div>
 	<vee-form :validation-schema="loginSchema" @submit="login">
 		<!-- Email -->
 		<div class="mb-3">
@@ -27,6 +30,7 @@
 		</div>
 		<button
 			type="submit"
+			:disabled="login_in_submission"
 			class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
                 hover:bg-purple-700"
 		>
@@ -44,10 +48,30 @@ export default {
         email: 'required|email',
         password: 'required|min:3|max:32',
       },
+      // it disables the submit button, while the form is submitting the data, to prevent excesive requests
+      login_in_submission: false,
+
+      // used to toggle the alert's visibility.
+      login_show_alert: false,
+
+      // will change the color of the alert.
+      login_alert_variant: 'bg-blue-500',
+
+      // the message inside of the alert.
+      login_alert_msg: 'Please wait! We are logging you in.',
     };
   },
   methods: {
     login(values) {
+      this.login_in_submission = true;
+      this.login_show_alert = true;
+      this.login_alert_variant = 'bg-blue-500';
+      this.login_alert_msg = 'Please wait! We are logging you in.';
+
+      //  create a dummy response to let the user know their login was a success.
+      this.login_alert_variant = 'bg-green-500';
+      this.login_alert_msg = 'Success! You are now logged in!';
+
       // this will help us verify that the function was executed if the validation was successful.
       console.log(values);
     },
