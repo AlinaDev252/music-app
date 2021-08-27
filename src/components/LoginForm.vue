12c360bb0e3adb85ffa36f4d01f13cae414a24e2
AlinaDev252 <costachealina25@gmail.com>
2021-08-27 11:20:57 +0300

    Redirect the user after logging out

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -24,7 +24,7 @@
 				name="password"
 				class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-				placeholder="Password"
+				placeholder="Password" autocomplete="on"
 			/>
 			<ErrorMessage class="text-red-600" name="password" />
 		</div>
