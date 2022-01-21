a5f8b5095475c2a8beba0f92b0fd8bc8679bdb83
AlinaDev252 <costachealina25@gmail.com>
2022-01-21 14:42:12 +0100

    Add final touches to the progress bar

--- src/store/index.js
+++ src/store/index.js
@@ -85,6 +85,9 @@ export default createStore({
       // }
     },
     async newSong({ commit, state, dispatch }, payload) {
+      if (state.sound instanceof Howl) {
+        state.sound.unload();
+      }
       commit('newSong', payload);
 
       state.sound.play();
