bcab758d3eee7e5c169c3f29315802afcb3946bc
AlinaDev252 <costachealina25@gmail.com>
2022-01-21 13:40:24 +0100

    Add current position and duration of a song

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,8 @@ export default createStore({
     signout: true,
     currentSong: {},
     sound: {},
+    seek: '00:00',
+    duration: '00:00',
   },
   mutations: {
     toggleAuthModal: (state) => {
@@ -23,6 +25,12 @@ export default createStore({
         src: [payload.url],
         html5: true,
       });
+    },
+    updatePosition(state) {
+      // the seek function will return the current position of the audio beeing played
+      state.seek = state.sound.seek();
+      // set the current duration of the song
+      state.duration = state.sound.duration();
     },
   },
   getters: {
@@ -73,10 +81,16 @@ export default createStore({
       //   payload.router.push({ name: 'home' });
       // }
     },
-    async newSong({ commit, state }, payload) {
+    async newSong({ commit, state, dispatch }, payload) {
       commit('newSong', payload);
 
       state.sound.play();
+
+      state.sound.on('play', () => {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      });
     },
     async toggleAudio({ state }) {
       if (!state.sound.playing) {
@@ -87,6 +101,15 @@ export default createStore({
         state.sound.pause();
       } else {
         state.sound.play();
+      }
+    },
+    progress({ commit, state, dispatch }) {
+      commit('updatePosition');
+
+      if (state.sound.playing()) {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
       }
     },
   },
