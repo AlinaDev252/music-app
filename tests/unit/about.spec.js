f42aa7165040da2de2e58943f2757a446c17c868
AlinaDev252 <costachealina25@gmail.com>
2022-02-14 13:58:57 +0100

    Add another method for testing the About component

--- tests/unit/about.spec.js
+++ tests/unit/about.spec.js
@@ -1,10 +1,10 @@
 import About from '@/views/About.vue';
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 
 // test suite
 describe('About.vue', () => {
   test('renders inner text', () => {
-    const wrapper = mount(About);
+    const wrapper = shallowMount(About);
 
     // we expect the text to contain the string 'about'
     expect(wrapper.text()).toContain('about');
