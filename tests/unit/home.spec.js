fce492d50453d3b96c7646f69a3de0447f6d3db0
AlinaDev252 <costachealina25@gmail.com>
2022-02-16 11:24:38 +0100

    Mock Firebase to fix some errors for the Home component test

--- tests/unit/home.spec.js
+++ tests/unit/home.spec.js
@@ -2,11 +2,18 @@ import { shallowMount } from '@vue/test-utils';
 import Home from '@/views/Home.vue';
 import SongItem from '@/components/SongItem.vue';
 
+// this function allows us to mock a dependency
+jest.mock('@/includes/firebase', () => {
+});
+
 describe('Home.vue', () => {
   test('renders list of songs', () => {
     const songs = [
       {}, {}, {},
     ];
+
+    // mock the method getSongs. We can mock methods in a component by overwriting it.
+    Home.methods.getSongs = () => false;
 
     const component = shallowMount(Home, {
       data() {
