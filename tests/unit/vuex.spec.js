e634c155fbb07d9c79ce67d4f50aab7f20cb9812
AlinaDev252 <costachealina25@gmail.com>
2022-02-17 14:52:12 +0100

    Add a test for Vuex mutations to check if the state was mutated correctly

---
+++ tests/unit/vuex.spec.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+import auth from '@/store/modules/auth';
+
+jest.mock('@/includes/firebase', () => {});
+
+describe('Vuex Store', () => {
+  test('toggleAuth mutation sets userLoggedIn to true', () => {
+    const store = createStore({
+      modules: {
+        auth,
+      },
+    });
+
+    expect(store.state.auth.userLoggedIn).not.toBe(true);
+    store.commit('toggleAuth');
+    expect(store.state.auth.userLoggedIn).toBe(true);
+  });
+});
